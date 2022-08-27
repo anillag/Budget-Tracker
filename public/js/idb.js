b2bc4dabc8f8b0adea90ff2ce82b298d52e48fdf
Anillag <101154249+anillag@users.noreply.github.com>
2022-08-27 17:03:51 -0400

    Added offline functionality with IndexedDB

---
+++ public/js/idb.js
@@ -0,0 +1,55 @@
+console.log("Loading idb.js here");
+
+const indexedDB =
+  window.indexedDB ||
+  window.mozIndexedDB ||
+  window.webkitIndexedDB ||
+  window.msIndexedDB ||
+  window.shimIndexedDB;
+
+let database;
+const request = indexedDB.open("budget-tracker", 1);
+
+request.onsuccess = ({ target }) => {
+  database = target.result;
+};
+
+request.onupgradeneeded = (event) => {
+  const database = event.target.result;
+  database.createObjectStore("savedData", { autoIncrement: true });
+};
+
+function saveRecord(savedRecord) {
+  const transaction = database.transaction(["savedData"], "readwrite");
+  const store = transaction.objectStore("savedData");
+
+  console.log("Saved in saveRecord: ", savedRecord);
+  store.add(savedRecord);
+}
+
+function checkDB() {
+  const transaction = database.transaction(["savedData"], "readwrite");
+  const store = transaction.objectStore("savedData");
+  const allRecords = store.getAll();
+
+  allRecords.onsuccess = () => {
+    if (allRecords.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(allRecords.result),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          const transaction = database.transaction(["savedData"], "readwrite");
+          const store = transaction.objectStore("savedData");
+          store.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener("online", checkDB);
